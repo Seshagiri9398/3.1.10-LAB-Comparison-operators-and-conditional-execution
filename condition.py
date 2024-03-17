e9fc069fc45b0034a73915148a60ead5e6b93fc3
Seshagiri9398 <161609180+Seshagiri9398@users.noreply.github.com>
2024-03-17 21:29:12 +0530

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,11 @@
+name=input("enter the flower name:")
+
+if name == "Spathiphyllum":
+      print("yes - Spathiphyllum is the best plant ever!")
+       
+elif name == "spathiphyllum":
+      print("no,i want a big Spathiphyllum!")
+              
+else:
+      print("Spathiphyllum! not pelargonium!")
+
